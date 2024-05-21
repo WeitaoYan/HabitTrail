195c74db65187609668c67365feac7380c733310
Yan Weitao <yanweitao@hotmail.com>
2024-05-21 21:32:40 +0800

    first commit

---
+++ GoodHabit/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for GoodHabit project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GoodHabit.settings')
+
+application = get_wsgi_application()
